736afc579c85386070f4b1464461aa1c24d4f0cf
BeaverStudio2007 <bo4rijst.pv@gmail.com>
2026-01-07 21:37:30 +0100

    YES

--- .eleventy.js
+++ .eleventy.js
@@ -16,7 +16,7 @@ module.exports = function(eleventyConfig) {
     // Добавляй сюда другие файлы/папки
   ];
   files.forEach(file => eleventyConfig.addPassthroughCopy(file));
-  eleventyConfig.addPassthroughCopy("src/fonts");
+  eleventyConfig.addPassthroughCopy("obsidian-to-blog/src/fonts");
 
   // Plugins
   eleventyConfig.addPlugin(pluginRss);
